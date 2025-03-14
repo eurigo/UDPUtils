1674fc1caa136a5356b6a02860b94ffdecb8c753
eurigo <418039625@qq.com>
2025-03-14 08:53:27 +0800

    build: 🔨️update gradle-8.9/AGP-8.7.3

--- app/src/main/java/com/eurigo/udputils/UdpActivity.java
+++ app/src/main/java/com/eurigo/udputils/UdpActivity.java
@@ -4,9 +4,14 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowInsets;
 import android.widget.EditText;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -33,16 +38,21 @@ public class UdpActivity extends AppCompatActivity implements
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.udp_receive_activity);
+
         initView();
-        mAdapter.addDataAndScroll("本机WiFi地址:  " + NetworkUtils.getIpAddressByWifi());
-        mAdapter.addDataAndScroll("本机Ipv4地址:  " + NetworkUtils.getIPAddress(true));
-        mAdapter.addDataAndScroll("本机广播地址:  " + UdpUtils.getInstance().getBroadcastHost(this));
+        mAdapter.addDataAndScroll("本机WiFi地址: " + NetworkUtils.getIpAddressByWifi());
+        mAdapter.addDataAndScroll("本机IPV4地址: " + NetworkUtils.getIPAddress(true));
+        mAdapter.addDataAndScroll("本机广播地址: " + UdpUtils.getInstance().getBroadcastHost(this));
+        mAdapter.addDataAndScroll("本机子网广播地址: " );
+        mAdapter.addDataAndScroll("本机组播地址: " );
+        mAdapter.addDataAndScroll("本机mDNS地址: 224.0.0.251:5353" );
         NetworkUtils.registerNetworkStatusChangedListener(this);
     }
-    
-    private void initView(){
+
+    private void initView() {
         btnReceive = findViewById(R.id.btn_udp_receive);
         btnSend = findViewById(R.id.btn_udp_send);
         btnReceive.setOnClickListener(this);
@@ -51,7 +61,7 @@ public class UdpActivity extends AppCompatActivity implements
         etSendPort = findViewById(R.id.et_send_port);
         etSendContent = findViewById(R.id.et_send_content);
         etReceivePort = findViewById(R.id.et_receive_port);
-        
+
         RecyclerView mRecyclerView = findViewById(R.id.rcv_udp_receive);
         mAdapter = new LogAdapter(new ArrayList<>());
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -61,56 +71,53 @@ public class UdpActivity extends AppCompatActivity implements
     @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_udp_send:
-                // 重置目标IP
-                String host = getEditText(etSendHost);
-                if (!UdpUtils.getInstance().isIpAddress(host)) {
-                    ToastUtils.showShort("请输入正确的IP地址！");
-                    return;
+        if (v.getId() == R.id.btn_udp_send) {
+            // 重置目标IP
+            String host = getEditText(etSendHost);
+            if (!UdpUtils.getInstance().isIpAddress(host)) {
+                ToastUtils.showShort("请输入正确的IP地址！");
+                return;
+            }
+            if (!TextUtils.isEmpty(host) && !host.equals(UdpUtils.getInstance().getCurrentHost())) {
+                UdpUtils.getInstance().setUdpHost(host);
+            }
+            // 重置目标端口
+            if (!TextUtils.isEmpty(getEditText(etSendPort))) {
+                int port = Integer.parseInt(getEditText(etSendPort));
+                if (port != UdpUtils.getInstance().getCurrentPort()) {
+                    UdpUtils.getInstance().setUdpPort(port);
                 }
-                if (!TextUtils.isEmpty(host) && !host.equals(UdpUtils.getInstance().getCurrentHost())) {
-                    UdpUtils.getInstance().setUdpHost(host);
+            }
+            if (TextUtils.isEmpty(getEditText(etSendContent))) {
+                ToastUtils.showShort("发送内容不能为空");
+                return;
+            }
+            UdpUtils.getInstance().sendMessage(getEditText(etSendContent));
+            mAdapter.addDataAndScroll(TimeUtils.getNowString()
+                    + "\n已向目标【" + UdpUtils.getInstance().getCurrentHost()
+                    + ":" + UdpUtils.getInstance().getCurrentPort() + "】发送数据: "
+                    + getEditText(etSendContent));
+            return;
+        }
+        if (v.getId() == R.id.btn_udp_receive) {
+            if ("开始接收".equals(btnReceive.getText().toString())) {
+                // 重置接收的监听端口
+                if (!TextUtils.isEmpty(etReceivePort.getText())) {
+                    UdpUtils.getInstance().setUdpPort(Integer.parseInt(getEditText(etReceivePort)));
+                    mAdapter.addDataAndScroll("开始接收"
+                            + UdpUtils.getInstance().getCurrentPort() + "端口数据包...");
+                    UdpUtils.getInstance().setReceiveListener(this);
+                    UdpUtils.getInstance().startUdpSocket();
+                    btnReceive.setText("停止接收");
+                    // 标记开始
+                    isStartUdp = true;
                 }
-                // 重置目标端口
-                if (!TextUtils.isEmpty(getEditText(etSendPort))) {
-                    int port = Integer.parseInt(getEditText(etSendPort));
-                    if (port != UdpUtils.getInstance().getCurrentPort()) {
-                        UdpUtils.getInstance().setUdpPort(port);
-                    }
-                }
-                if (TextUtils.isEmpty(getEditText(etSendContent))) {
-                    ToastUtils.showShort("发送内容不能为空");
-                    return;
-                }
-                UdpUtils.getInstance().sendMessage(getEditText(etSendContent));
-                mAdapter.addDataAndScroll(TimeUtils.getNowString()
-                        + "\n已向目标【" + UdpUtils.getInstance().getCurrentHost()
-                        + ":" + UdpUtils.getInstance().getCurrentPort() + "】发送数据: "
-                        + getEditText(etSendContent));
-                break;
-            case R.id.btn_udp_receive:
-                if ("开始接收".equals(btnReceive.getText().toString())) {
-                    // 重置接收的监听端口
-                    if (!TextUtils.isEmpty(etReceivePort.getText())) {
-                        UdpUtils.getInstance().setUdpPort(Integer.parseInt(getEditText(etReceivePort)));
-                        mAdapter.addDataAndScroll("开始接收"
-                                + UdpUtils.getInstance().getCurrentPort() + "端口数据包...");
-                        UdpUtils.getInstance().setReceiveListener(this);
-                        UdpUtils.getInstance().startUdpSocket();
-                        btnReceive.setText("停止接收");
-                        // 标记开始
-                        isStartUdp = true;
-                    }
-                }else {
-                    UdpUtils.getInstance().stopUdpSocket();
-                    btnReceive.setText("开始接收");
-                    // 标记结束
-                    isStartUdp = false;
-                }
-                break;
-            default:
-                break;
+            } else {
+                UdpUtils.getInstance().stopUdpSocket();
+                btnReceive.setText("开始接收");
+                // 标记结束
+                isStartUdp = false;
+            }
         }
     }
 
@@ -156,7 +163,7 @@ public class UdpActivity extends AppCompatActivity implements
         UdpUtils.getInstance().stopUdpSocket();
     }
 
-    private String getEditText(EditText editText){
+    private String getEditText(EditText editText) {
         return editText.getText().toString().trim();
     }
 }
